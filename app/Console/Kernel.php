64cabe560bdb1c81d821f066de8eb26e4e7b0ece
eliyajoseph7 <exaveryeliya20@gmail.com>
2024-03-29 20:27:49 +0300

    modified

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('monthly:report')->dailyAt('20:16');
+        $schedule->command('monthly:report')->dailyAt('20:30');
     }
 
     /**
